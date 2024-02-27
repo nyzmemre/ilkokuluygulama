07242adcce14de80e9f91b3f98b258bdba572a98
nyzmemre <nyz_memre_09@hotmail.com>
2024-02-27 04:18:27 +0300

    added some page

---
+++ lib/features/count_with_ball_page.dart
@@ -0,0 +1,38 @@
+/*
+import 'package:flutter/material.dart';
+import 'package:ilkokuluygulama/product/utility/widgets/my_scaffold.dart';
+
+class CountWithBallPage extends StatelessWidget {
+   CountWithBallPage({Key? key}) : super(key: key);
+
+  int rakam=5;
+List<int> indexRast=[0,1,2,3,4,5,6,7,8,9,10,11,12];
+
+  @override
+  Widget build(BuildContext context) {
+    indexInfo=List.generate(indexRast.length, (index) => )
+    return MyScaffold(child: SingleChildScrollView(
+      child: Column(
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Container(height: 200, width: 200, color: Colors.grey.shade200,
+
+                child: Stack(
+                  children: [
+                    Positioned(
+                        left: ,
+                        child: ListView.builder(
+                      itemCount:3,
+                      itemBuilder: (context, int index) {
+                        return CircleAvatar();
+                      },
+                    ))
+                  ],
+                ),)),
+        ],
+      ),
+    ));
+  }
+}
+*/
