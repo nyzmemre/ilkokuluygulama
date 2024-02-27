166203c8a288ce7529c393b48d4a4694e4b5ca70
nyzmemre <nyz_memre_09@hotmail.com>
2024-02-27 23:48:40 +0300

    add some changes

--- lib/features/count_with_ball_page.dart
+++ lib/features/count_with_ball_page.dart
@@ -1,5 +1,8 @@
-/*
+
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:ilkokuluygulama/product/utility/widgets/my_scaffold.dart';
 
 class CountWithBallPage extends StatelessWidget {
@@ -10,10 +13,34 @@ List<int> indexRast=[0,1,2,3,4,5,6,7,8,9,10,11,12];
 
   @override
   Widget build(BuildContext context) {
-    indexInfo=List.generate(indexRast.length, (index) => )
+    int index=Random().nextInt(21);
+    (index==0) ? index=1 : index=index;
+    int second=Random().nextInt(21);
     return MyScaffold(child: SingleChildScrollView(
       child: Column(
         children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width-40,
+            height: 300,//(index<7) ? index*50 : index*20,
+            child: GridView.builder(
+              shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                physics: ScrollPhysics(),
+                itemCount: index,
+                itemBuilder: (context, int index){
+              return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.blue,
+                      ),
+                    ),
+                  );
+            }, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: (index>7) ? 7 : index),),
+          ),
+          Text(index.toString()),
           ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: Container(height: 200, width: 200, color: Colors.grey.shade200,
@@ -21,18 +48,23 @@ List<int> indexRast=[0,1,2,3,4,5,6,7,8,9,10,11,12];
                 child: Stack(
                   children: [
                     Positioned(
-                        left: ,
+
                         child: ListView.builder(
-                      itemCount:3,
+                      itemCount:index,
                       itemBuilder: (context, int index) {
-                        return CircleAvatar();
+                        return SizedBox(
+                            width: 20,
+                            child: CircleAvatar());
                       },
                     ))
                   ],
                 ),)),
+          SizedBox(height: 20,),
+          Text(second.toString()),
+
         ],
       ),
     ));
   }
 }
-*/
+
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ilkokuluygulama/features/count_with_ball_page.dart';
 import 'package:ilkokuluygulama/read_view.dart';
 import 'package:ilkokuluygulama/substraction_view.dart';
 
@@ -11,38 +12,30 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(title: Text('Alıştırmalar'),),
       body: Column(
         children: [
-          InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (_)=>ReadView()));
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Card(
-                margin: EdgeInsets.all(10),
-                child: Center(child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text('Okuma Hızlandırma',),
-                )),
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (_)=>SubstractionView()));
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Card(
-                margin: EdgeInsets.all(10),
-                child: Center(child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text('Çıkarma',),
-                )),
-              ),
-            ),
-          )
+          page_button(context,ReadView() ,'Okuma Hızlandırma'),
+          page_button(context,SubstractionView() ,'Çıkarma'),
+          page_button(context,CountWithBallPage() ,'Toplama'),
+
         ],
       ),
     );
+  }
+
+  InkWell page_button(BuildContext context, Widget widget, String text) {
+    return InkWell(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (_)=>widget));
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Card(
+              margin: EdgeInsets.all(10),
+              child: Center(child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(text),
+              )),
+            ),
+          ),
+        );
   }
 }
